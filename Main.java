a00427a854d0c268961f79966717709429a14cbb
DetelinaZheynova <126780362+DetelinaZheynova@users.noreply.github.com>
2023-04-17 10:06:56 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
-//Проект 3: XML Parser 
+//Проект 3: XML Parser
         public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
+//<people> <person id=”0”>
+//<name>John Smith</name>
+//<address>USA</address> </person> <person id=”1”>
+//<name>Ivan Petrov</name> <address>Bulgaria</address> </person> </people
