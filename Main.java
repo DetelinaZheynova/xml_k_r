6469e17ed741b9d2baee0f78e9a0093353e32739
DetelinaZheynova <126780362+DetelinaZheynova@users.noreply.github.com>
2023-04-23 12:17:43 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,4 +7,53 @@
 //<people> <person id=”0”>
 //<name>John Smith</name>
 //<address>USA</address> </person> <person id=”1”>
-//<name>Ivan Petrov</name> <address>Bulgaria</address> </person> </people
+//<name>Ivan Petrov</name> <address>Bulgaria</address> </person>
+// <person id=”2”>
+////<name>Peter Pelov</name> <address>Bolivia</address> </person>
+// <person id=”3”>
+////<name>Ivo Kirov</name> <address>Panama</address> </person>
+// <person id=”4”>
+////<name>Iva Ivailova</name> <address>Portugal</address> </person>
+// <person id=”5”>
+////<name>George Pesov</name> <address>Argentina</address> </person>
+// <person id=”6”>
+////<name>Aia Vernice</name> <address>Venezuela</address> </person>
+//<person id=”7”>
+////<name>Alen Lake</name> <address>Latvia</address> </person>
+// <person id=”8”>
+////<name>Ivak Potter</name> <address>San Antonia</address> </person>
+//<person id=”9”>
+////<name>Mek Marcine</name> <address>France</address> </person>
+// <person id=”10”>
+////<name>Sart Kasov</name> <address>Bangladesh</address> </person>
+// <person id=”11”>
+////<name>John Delvers</name> <address>San Francisco</address> </person>
+// <person id=”12”>
+////<name>Peter Talen</name> <address>Serbia</address> </person>
+// <person id=”13”>
+////<name>Saf Paset</name> <address>Canada</address> </person>
+// <person id=”14”>
+////<name>Silvi Vaset</name> <address>Texas</address> </person>
+//<person id=”15”>
+////<name>Evie Albarn</name> <address>Spain</address> </person>
+// <person id=”16”>
+////<name>Claudia Petrolova</name> <address>Hungary</address> </person>
+// <person id=”17”>
+////<name>Mirajane Savol</name> <address>Switzerland</address> </person>
+// <person id=”18”>
+////<name>Sarah Starfel</name> <address>Greece</address> </person>
+// <person id=”19”>
+////<name>Vivian Shanes</name> <address>Italy</address> </person>
+// <person id=”20”>
+////<name>Violet Sinclair</name> <address>Latvia</address> </person>
+//<person id=”21”>
+////<name>Will Brom</name> <address>Estonia</address> </person>
+// <person id=”22”>
+////<name>Bernard Samsen</name> <address>Poland</address> </person>
+// <person id=”23”>
+////<name>Samvel Jayce</name> <address>Germany</address> </person>
+// <person id=”24”>
+////<name>Insil Devol</name> <address>Belgium</address> </person>
+// <person id=”25”>
+////<name>Shamel Heisenberg</name> <address>Denmark</address> </person>
+// </people>
